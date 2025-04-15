7b3e7933b019b5234f43b59f14423caa0111c2fb
Ekaterina Sevciuc <207343906+kate8382@users.noreply.github.com>
2025-04-15 17:42:47 +0300

    added to_do_list files

---
+++ eslint.config.cjs
@@ -0,0 +1,63 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+// const path = require('path');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+module.exports = [
+  ...compat.config({
+    extends: ['airbnb-base'],
+
+    env: {
+      browser: true, // Для работы с document, window, localStorage
+      es2021: true, // Для поддержки современных возможностей JavaScript
+    },
+
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    rules: {
+      // Разрешить использование консоли
+      'no-console': 'off',
+
+      // Отключить требование именования функций
+      'func-names': 'off',
+
+      // Требовать использование одинарных кавычек
+      quotes: ['error', 'single'],
+
+      // Требовать точки с запятой
+      semi: ['error', 'always'],
+
+      // Запретить неиспользуемые переменные
+      'no-unused-vars': ['warn'],
+
+      // Отступы в 2 пробела
+      indent: ['error', 2],
+
+      // Запретить использование var
+      'no-var': 'error',
+
+      // Запретить использование неявного приведения типов
+      'linebreak-style': ['error', 'windows'],
+
+      // Пробелы вокруг ключевых слов
+      'keyword-spacing': ['error', { before: true, after: true }],
+
+      // Пробелы вокруг операторов
+      'no-restricted-globals': 'off',
+
+      // Pазрешить использование alert
+      'no-alert': 'off',
+
+      // Разрешить использование ++
+      'no-plusplus': 'off',
+
+      // Отключить проверку на использование зависимостей из devDependencies
+      'import/no-extraneous-dependencies': 'off',
+    },
+  }),
+];
